4d91df580ee73ae00e62419f061ba081c7736fbd
robz-tirtlib <62515024+robz-tirtlib@users.noreply.github.com>
2023-11-03 17:38:51 +0300

    Added user creation. Username and emails uniqueness are checked on registration

--- workshop/users/views.py
+++ workshop/users/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
 
+from django.contrib.auth.models import User
+
 from .forms import RegisterForm
 
 
@@ -14,12 +16,25 @@ def register(request):
         form = RegisterForm(request.POST)
 
         if form.is_valid():
-            print(form.__dict__)
+            if User.objects.filter(username=form.cleaned_data["username"]):
+                form.add_error("username", "This field should be unique.")
+                return render(request, "users/register.html", {"form": form})
+
+            if User.objects.filter(email=form.cleaned_data["email"]):
+                form.add_error("email", "This field should be unique.")
+                return render(request, "users/register.html", {"form": form})
+
+            user = User.objects.create_user(
+                username=form.cleaned_data["username"],
+                email=form.cleaned_data["email"],
+                password=form.cleaned_data["password"]
+                )
+            user.save()
             context = {}
             return redirect(reverse("workshopapp:index", kwargs=context))
+        return render(request, "users/register.html", {"form": form})
 
-    context = {}
-    return render(request, "users/register.html", context)
+    return render(request, "users/register.html", {"form": RegisterForm()})
 
 
 def profile(request, user_id: int):
